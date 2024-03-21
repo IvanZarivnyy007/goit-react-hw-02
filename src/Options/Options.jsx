08bda52ddbc649e3fbe8a524311f19bc68fea11a
IvanZarivnyy007 <144140704+IvanZarivnyy007@users.noreply.github.com>
2024-03-21 23:52:59 +0100

    1
    2

---
+++ src/Options/Options.jsx
@@ -0,0 +1,10 @@
+export const Options = () => {
+  return (
+    <>
+      <button className="buton">Good</button>
+      <button className="buton">Neutral</button>
+      <button className="buton">Bad</button>
+      <button className="buton">Reset</button>
+    </>
+  );
+};
